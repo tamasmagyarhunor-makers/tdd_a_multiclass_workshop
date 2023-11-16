e5799f514d3d22584219a4de7e32dd7a029e2026
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-11-16 10:36:12 +0000

    Write new tests, pass

---
+++ lib/house.py
@@ -0,0 +1,5 @@
+class House():
+    def __init__(self, bedrooms, bathrooms):
+        self.bedrooms = bedrooms
+        self.bathrooms = bathrooms
+        self.persons = []
---
+++ tests/test_house.py
@@ -0,0 +1,31 @@
+from lib.house import *
+import pytest
+
+def test_house_can_be_instantiated():
+    actual = House(2, 3)
+
+    assert isinstance(actual, House)
+
+def test_initialises_with_number_of_bedrooms_and_bathrooms():
+    # situation
+    house = House(2, 3)
+    
+    # action
+    actual_bedrooms = house.bedrooms
+    actual_bathrooms = house.bathrooms
+
+    expected_bedrooms = 2
+    expected_bathrooms = 3
+
+    # assertion
+    assert actual_bedrooms == expected_bedrooms
+    assert actual_bathrooms == expected_bathrooms
+
+def test_house_instantiates_with_ability_to_store_persons():
+    house = House(1, 1)
+
+    actual = house.persons
+
+    expected = []
+
+    assert actual == expected
